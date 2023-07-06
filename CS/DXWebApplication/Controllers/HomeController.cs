81a33e0b26009b5e75853e726e079c089902c92a
nikulina.svetlana <svetlana.nikulina@devexpress.com>
2023-07-06 16:55:36 +0400

    upd code

---
+++ CS/DXWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DXWebApplication.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!";
+
+            return View();
+        }
+        public ActionResult GridViewPartial() {
+            var model = Enumerable.Range(0, 10).Select(i => new { ID = i, Group = i % 2 == 0 ? "Group 1" : "Group 2" });
+            return PartialView(model);
+        }
+    }
+}
--- CS/Q514574/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Codebehind="Global.asax.cs" Inherits="Q514574.MvcApplication" Language="C#" %>
